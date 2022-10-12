075e6a5961351aae686145972ba4bcc92b454569
Ibby <theibbster@protonmail.com>
2022-10-12 22:16:27 +0100

    Add more functions for reading files

--- BasicFileReading/read-greetings.c
+++ BasicFileReading/read-greetings.c
@@ -1,16 +1,85 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void main() {
-    char i;
+FILE * openFile() {
+    FILE *fP;
+    fP = fopen("greetings.txt", "r");
+    return fP;
+}
+
+/**
+ * @brief Print each line of the file individually
+ * 
+ */
+void lineByLine(FILE * fP) {
+    char buff[255];
+    int count = 1;
+    while (fgets(buff,255, (FILE *)fP) != NULL ) {
+        printf("%d: %s", count, buff);
+        count++;
+    }
+}
+
+/**
+ * @brief Print each character separated by a comma
+ * 
+ */
+void charByChar(FILE * fP) {
+    char c;
+    while (!feof(fP)) {
+        c = fgetc(fP);
+        printf("%c", c);
+        if(c!='\n') {
+            printf(", ");
+        }
+    }
+    printf("\n");
+}
+
+/**
+ * @brief Print contents of file without any spaces or new lines.
+ *  
+ */
+void stripSpace(FILE * fP) {
+    char c;
+    // Loop until you get to end of file
+    while (!feof(fP)) {
+        c = fgetc(fP);
+        if(c!='\n' && c!=' ') {
+            printf("%c", c);
+        }
+    }
+    printf("\n");    
+}
+
+/**
+ * @brief Count number of lines in the file
+ * 
+ */
+void countLines(FILE * fP) {
+    char buff[255];
+    int count = 0;
+    // Loop until end of file
+    while (!feof(fP)) {
+        // printf("%d: %s", count, buff);
+        fgets(buff,255, (FILE *)fP);
+        count++;
+    }
+    printf("There are %d greetings.", count);
+}
+
+int main() {
     FILE *fGreetings;
-    fGreetings = fopen("greetings.txt", "r");
+    fGreetings = openFile();
     // Exit if file doesn't exist
     if (fGreetings == NULL) {
         perror("There was an error opening the file");
         exit(EXIT_FAILURE);
     }
-    fscanf(fGreetings, "%c", &i);
-    printf("The first character of the file is %c\n", i);
+    // lineByLine(fGreetings);
+    // charByChar(fGreetings);
+    // stripSpace(fGreetings);
+    countLines(fGreetings);
     fclose(fGreetings);
+    return 0;
 }
